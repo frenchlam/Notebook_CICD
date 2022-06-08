ae5cd3096f1f7f6184b2279466a777bf44c69ddb
frenchlam <frenchlam>
2022-06-08 21:09:29 +0000

    working version

--- Exploration.py
+++ Exploration.py
@@ -38,101 +38,32 @@ demographics_df = spark.read.table("matthieulamDAIWT.demographic")
 enriched_df = compute_service_features(spark.read.table("matthieulamDAIWT.other_features"))
 demographic_service_df = demographics_df.join(enriched_df, demographics_df.customerID == enriched_df.customerID, "inner").drop(enriched_df.customerID)
 
+demographic_service_df.drop("customerID").write.mode("overwrite").format("delta").saveAsTable("matthieulamDAIWT.demographic_service")
+
 display(demographic_service_df)
 
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC 
-# MAGIC ## Exploration with the Feature Store
-# MAGIC 
-# MAGIC The model was OK, but, could probably be better with more data. There is, fortunately, more data about customers available -- additional information about the services they use, as well as some other derived, aggregated data. This was (let us say) previously used for another customer-related modeling task. This data is therefore available in the Feature Store. Why not reuse these features and see what happens, before going further?
-# MAGIC 
-# MAGIC Use the Feature Store to read and join everything in the `service_features` feature table.
+demographic_service_df.printSchema()
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC Save the augmented data set as a table, for auto ML.
+# MAGIC %md 
+# MAGIC ## Altenatively use Feature Store
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC DROP TABLE if exists matthieulamDAIWT.demographic_service
+from databricks.feature_store import FeatureStoreClient, FeatureLookup
 
-# COMMAND ----------
+fs = FeatureStoreClient()
+training_set = fs.create_training_set(spark.read.table("matthieulamDAIWT.demographic"), 
+                                      [FeatureLookup(table_name = "matthieulamDAIWT.service_features", lookup_key="customerID")], 
+                                      label=None, exclude_columns="customerID")
 
-training_set.load_df().write.format("delta").saveAsTable("matthieulamDAIWT.demographic_service")
+training_set.load_df().write.mode("overwrite").format("delta").saveAsTable("matthieulamDAIWT.demographic_service")
 
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC # Model Insights with SHAP
-# MAGIC 
-# MAGIC As part of the exploration process, having a baseline model early helps explore the _data_ in turn. For example, the basic SHAP plots created by auto ML can be expanded to explore more of the data:
-
-# COMMAND ----------
-
-# MAGIC %pip install scikit-learn==1.0
-
-# COMMAND ----------
-
-import mlflow
-import mlflow.sklearn
-from shap import KernelExplainer, summary_plot
-from sklearn.model_selection import train_test_split
-import pandas as pd
-
-mlflow.autolog(disable=True)
-
-sample = spark.read.table("matthieulamDAIWT.demographic_service").sample(0.05).toPandas()
-data = sample.drop(["Churn"], axis=1)
-labels = sample["Churn"]
-X_background, X_example, _, y_example = train_test_split(data, labels, train_size=0.5, random_state=42, stratify=labels)
-
-model = mlflow.sklearn.load_model("runs:/b53bf168667a46aaadb6a06aab0bc0ac/model")
-
-predict = lambda x: model.predict_proba(pd.DataFrame(x, columns=X_example.columns))[:,-1]
-explainer = KernelExplainer(predict, X_example)
-shap_values = explainer.shap_values(X=X_example)
-
-# COMMAND ----------
-
-summary_plot(shap_values, features=X_example)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ... or explore how churn factors differ by gender:
-
-# COMMAND ----------
-
-from shap import group_difference_plot
-
-group_difference_plot(shap_values[y_example == 1], X_example[y_example == 1]['gender'] == 'Male', feature_names=X_example.columns)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ... or 'cluster' customers by their model explanations and see whether patterns emerge. There are clearly a group of churners that tend to be on 1-month contracts, short tenure, and equally a cluster of non-churners who are on 2-year contracts and have been long time customers. But we knew that!
-
-# COMMAND ----------
-
-import seaborn as sns
-from sklearn.manifold import TSNE
-
-embedded = TSNE(n_components=2, learning_rate='auto', init='pca', random_state=42).fit_transform(shap_values)
-
-sns.set(rc = {'figure.figsize':(16,9)})
-sns.scatterplot(x=embedded[:,0], y=embedded[:,1], \
-                style=X_example['Contract'], \
-                hue=X_example['tenure'], \
-                size=(y_example == 1), size_order=[True, False], sizes=(100,200))
+display(training_set.load_df())
 
 # COMMAND ----------
 
